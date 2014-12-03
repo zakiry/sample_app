d5179274e4083b459e03f13a14614c2e0c209ab2
Ryo Jozaki <zaki0ry@gmail.com>
2014-12-03 23:22:20 +0900

    add about

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,4 +15,11 @@ describe "Static Pages" do
       expect(page).to have_content('Help')
     end
   end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
