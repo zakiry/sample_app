f369f37f7a8416aaa0877cec44027c7aae9300bc
Ryo Jozaki <zaki0ry@gmail.com>
2014-12-03 21:44:43 +0900

    add home and help

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+    it " should have the contet 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+end
